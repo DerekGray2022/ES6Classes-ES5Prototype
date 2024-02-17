92fda745b37f07f2d62417e349d3e764760e9af0
Derek Gray <105393523+DerekGray2022@users.noreply.github.com>
2024-02-17 14:40:21 +0000

    ES5 Prototype

--- script.js
+++ script.js
@@ -1,54 +1,55 @@
+//      ES5 Prototype Method
 let users = [];
 
-class User {
-    constructor(email, name) {
-        this.email = email;
-        this.name = name;
-        this.score = 0;
-        users.push(this);
-    };
-
-    logIn() {
-        console.log(`${this.name} has logged IN.`);
-        return this;
-    };
-
-    logOut() {
-        console.log(`${this.name} has logged OUT.`);
-        return this;
-    };
-
-    updateScore() {
-        this.score++;
-        console.log(`${this.email}'s score is now ${this.score}`);
-        return this;
-    };
+//      Constructor
+function User(email, name) {
+    this.name = name;
+    this.email = email;
+    this.online = false;
+    users.push(this);
 };
 
-class Admin extends User {
-    deleteUser(user) {
-        users = users.filter(u => {
-            return u.email !== user.email;
-        });
-    };
+//      Prototype Constructor
+User.prototype.logIn = function () {
+    this.online = true;
+    console.log(`${this.name} has logged IN.`);
 };
+
+User.prototype.logOut = function() {
+    this.online = false;
+    console.log(`${this.name} has logged OUT.`);
+};
+
+//      Inheritance
+function Admin(...args) {
+    User.apply(this, args);
+    this.role = 'Administrator';
+};
+//      Prototypes
+Admin.prototype = Object.create(User.prototype);
+Admin.prototype.deleteUser = function (user) {
+    users = users.filter(u => {
+        if (u.name === user.name) {
+            console.log(`${user.name} has been deleted.`);
+        }
+        return u.name !== user.name;
+    });
+};
+
 
 const user01 = new User('lou@velvets.com', 'Lou Reed');
 const user02 = new User('john@velvets.com', 'John Cale');
-const admin = new Admin('sterling@velvets.com', 'Sterling Morrison');
+const user03 = new User('sterling@velvets.com', 'Sterling Morrison');
+const admin = new Admin('moe@velvets.com', 'Moe Tucker');
 
 console.log(users);
-
-console.log(user01);
-console.log(user02);
-console.log(admin);
-
-user01.logIn().updateScore().updateScore().logOut();
+user02.logIn();
+admin.logIn();
 admin.deleteUser(user02);
-
 console.log(users);
-const myUser = Object.getPrototypeOf(user01);
-console.log(myUser);
+admin.logOut();
+
+
 
 
 
